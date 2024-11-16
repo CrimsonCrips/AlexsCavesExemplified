b3c3a161cff5e4e01c10431dc269cb277afc6951
Crim <danecarmona2@gmail.com>
2024-11-16 18:22:28 +0800

    NoMobSpawning

--- src/main/java/org/crimsoncrips/alexscavesexemplified/AlexsCavesExemplified.java
+++ src/main/java/org/crimsoncrips/alexscavesexemplified/AlexsCavesExemplified.java
@@ -11,8 +11,6 @@ import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.config.ModConfigEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.crimsoncrips.alexscavesexemplified.config.ACEConfigHolder;
-import org.crimsoncrips.alexscavesexemplified.config.ACExemplifiedConfig;
 import org.crimsoncrips.alexscavesexemplified.event.ACExemplifiedEvents;
 
 import java.util.Locale;
@@ -24,32 +22,15 @@ public class AlexsCavesExemplified {
 
     public AlexsCavesExemplified() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-        modEventBus.addListener(this::onModConfigEvent);
         MinecraftForge.EVENT_BUS.register(new ACExemplifiedEvents());
         MinecraftForge.EVENT_BUS.register(this);
         modEventBus.addListener(this::setup);
 
-        // Register our mod's ForgeConfigSpec so that Forge can create and load the config file for us
-        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, ACEConfigHolder.EXEMPLIFIED_SPEC, "alexscavesexemplified.toml");
-
     }
 
-
-
-    @SubscribeEvent
-    public void onModConfigEvent(final ModConfigEvent event) {
-        final ModConfig config = event.getConfig();
-        // Rebake the configs when they change
-        if (config.getSpec() == ACEConfigHolder.EXEMPLIFIED_SPEC) {
-            ACExemplifiedConfig.bake();
-        }
-    }
 
     private void setup(final FMLCommonSetupEvent event) {
     }
 
 
-    public static ResourceLocation prefix(String name) {
-        return new ResourceLocation(MODID, name.toLowerCase(Locale.ROOT));
-    }
 }
--- src/main/java/org/crimsoncrips/alexscavesexemplified/event/ACExemplifiedEvents.java
+++ src/main/java/org/crimsoncrips/alexscavesexemplified/event/ACExemplifiedEvents.java
@@ -1,5 +1,16 @@
 package org.crimsoncrips.alexscavesexemplified.event;
 
+import com.github.alexmodguy.alexscaves.server.entity.ACEntityDataRegistry;
+import com.github.alexmodguy.alexscaves.server.entity.ACEntityRegistry;
+import com.github.alexmodguy.alexscaves.server.level.biome.ACBiomeRegistry;
+import com.github.alexmodguy.alexscaves.server.misc.ACTagRegistry;
+import net.minecraft.core.Holder;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.level.biome.Biome;
+import net.minecraftforge.event.entity.living.MobSpawnEvent;
+import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import org.crimsoncrips.alexscavesexemplified.AlexsCavesExemplified;
 
@@ -7,6 +18,17 @@ import org.crimsoncrips.alexscavesexemplified.AlexsCavesExemplified;
 public class ACExemplifiedEvents {
 
 
+    @SubscribeEvent
+    public void mobSpawn(MobSpawnEvent.PositionCheck spawnPlacementCheck){
+        EntityType<?> entityType = spawnPlacementCheck.getEntity().getType();
+        Holder<Biome> biome = spawnPlacementCheck.getLevel().getBiome(spawnPlacementCheck.getEntity().blockPosition());
+        long time = spawnPlacementCheck.getLevel().dayTime();
 
+        String namespace = EntityType.getKey(entityType).getNamespace();
+
+        if(!namespace.equals("alexscaves")){
+            spawnPlacementCheck.setResult(Event.Result.DENY);
+        }
+    }
 
 }
